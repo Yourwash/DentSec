17add414ec25c0913cc88517ea7638de59217b61
Yourwash C. Azakas <georgeazakas@outlook.com>
2023-01-03 09:18:04 +0200

    Create DentSec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LinkComponent } from './link/link.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LinkComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/link/link.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-link',
+  templateUrl: './link.component.html',
+  styleUrls: ['./link.component.css']
+})
+export class LinkComponent implements OnInit{
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+  }
+  navigateFunction(){
+    this.router.navigateByUrl('/contact');
+  }
+
+}
